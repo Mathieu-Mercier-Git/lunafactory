e9c93f4cad63ffefea740292306aca9ea6533275
Mathieu-Mercir-Git <math_mercier@hotmail.fr>
2019-11-02 20:45:49 +0100

    This commit will add the methods to sort the list of products, and the method to show an html page using java

--- src/main/java/com/exercise/lunafactory/controller/ProductController.java
+++ src/main/java/com/exercise/lunafactory/controller/ProductController.java
@@ -6,48 +6,136 @@ import com.exercise.lunafactory.model.ProductList;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.tools.javac.util.ArrayUtils;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class ProductController {
 
-    //private String jsonStr = "[{\"id\":1,\"ean\":\"3484611\",\"name\":\"ABSORB\",\"weight\":59.17,\"description\":\"leathercare set\",\"price\":996.01,\"assembled\":false,\"dimension\":{\"width\":25.23,\"depth\":60.85,\"height\":5.33}},{\"id\":2,\"ean\":\"2246942\",\"name\":\"ABSORB\",\"weight\":53.62,\"description\":\"leathercleaner\",\"price\":404.07,\"assembled\":false,\"dimension\":{\"width\":61.47,\"depth\":50.61,\"height\":18.45}},{\"id\":3,\"ean\":\"1655204\",\"name\":\"ADMETE RUND\",\"weight\":96.61,\"description\":\"chair pad\",\"price\":979.7,\"assembled\":true,\"dimension\":{\"width\":49.88,\"depth\":82.89,\"height\":94.67}}]";
-    //private String jsonStr = "[{\"id\":1,\"ean\":\"3484611\",\"name\":\"ABSORB\",\"weight\":59.17,\"description\":\"leathercare set\",\"price\":996.01,\"assembled\":false,\"dimension\":{\"width\":25.23,\"depth\":60.85,\"height\":5.33}}]";
+    @RequestMapping(value="/ProductsTest")
+    public ModelAndView greeting() {
+
+        //ModelAndView model = new ModelAndView("index.html");
+        ModelAndView modelAndView = new ModelAndView("index.html");
+        return modelAndView;
+
+        //return "index.html";
+    }
+
     //curl -X GET "http://factory.lunatech.fr/products" -H "accept: application/json" -H "X-API-LunaFactory: mathieu.mercier77360@gmail.com:12c71b271acfc1bf"
 
     //@RequestMapping(value="/Produits",produces = MediaType.APPLICATION_JSON_VALUE,method=RequestMethod.GET)
-    @RequestMapping(value="/Produits",method=RequestMethod.GET)
-    public String listeProduits() {
+    @RequestMapping(value="/Products",method=RequestMethod.GET)
+    public String sortProducts() {
+        List<Product> productList = productList();
+        List<Product> sortedProductList = new ArrayList<>();
+        String result = "";
 
+
+        for (Product prod : productList) {
+            if (prod.isAssembled()) {
+                sortedProductList.add(prod);
+                result += prod.getName()+" / ";
+            }
+        }
+        return topPrices();
+    }
+
+    public String sortProductsWithNoDuplicates() {
+        boolean removeDup=true;
+
+        List<Product> productList = productList();
+        List<Product> sortedProductList = new ArrayList<>();
+        String result = "";
+
+        for(int i=0;i<productList.size();i++){
+            Product prod = productList.get(i);
+            if(prod.isAssembled()){
+                if(i==0){
+                    sortedProductList.add(prod);
+                    result += prod.getName()+" / ";
+                } else if (!prod.getName().equals(productList.get(i-1).getName())){
+                    sortedProductList.add(prod);
+                    result += prod.getName()+" / ";
+                }
+
+            }
+        }
+        return result;
+    }
+
+    public String topPrices(){
+        String result="";
+        List<Product> productList = new LinkedList<>();
+        productList.addAll(productList());
+        List<Product> topPricesList = new ArrayList<>();
+
+        for(int i=0;i<15;i++){
+            Product maxPriceProduct = getMaxPriceProd(productList);
+            topPricesList.add(maxPriceProduct);
+            result+=maxPriceProduct.getPrice()+" / ";
+            if (productList.contains(maxPriceProduct)){
+                productList.remove(maxPriceProduct);
+            }
+
+        }
+        return result;
+    }
+
+    public Product getMaxPriceProd(List<Product> prodList){
+        Product maxPriceProd= prodList.iterator().next();
+        double maxPrice = maxPriceProd.getPrice();
+        for(Product prod : prodList){
+            if(prod.getPrice()>maxPrice){
+                maxPriceProd = prod;
+                maxPrice = prod.getPrice();
+            }
+        }
+        return maxPriceProd;
+    }
+
+    public List<Product> productList(){
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-API-LunaFactory", "mathieu.mercier77360@gmail.com:12c71b271acfc1bf");
-
-
         HttpEntity<String> entity = new HttpEntity<>("body", headers);
 
         ResponseEntity<String> responseEntity = restTemplate.exchange("http://factory.lunatech.fr/products", HttpMethod.GET, entity, String.class);
-        //ProductList responseEntity = restTemplate.getForObject("http://factory.lunatech.fr/products", ProductList.class, entity);
         String responseContent = responseEntity.getBody();
         ObjectMapper mapper = new ObjectMapper();
         List<Product> productList;
         try {
+            //new LinkedList<String>(Arrays.asList(split)); CAN BE USED
             productList = Arrays.asList(mapper.readValue(responseContent, Product[].class));
-            //productList = mapper.readValue(responseContent, ProductList.class);
         } catch (JsonProcessingException e) {
             productList = null;
             e.printStackTrace();
         }
 
-        /*//Product(int id, String ean, String name, double weigth, String description, double price, boolean assembled, Dimension dimension)
+        return productList;
+    }
+
+    /*@RequestMapping("/products")
+    public List<Product> getProductList(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        ProductList response = restTemplate.getForObject(
+                "http://factory.lunatech.fr/products",
+                ProductList.class);
+        List<Product> products = response.getProducts();
+
+        return products;
+    }*/
+
+    /*//Product(int id, String ean, String name, double weigth, String description, double price, boolean assembled, Dimension dimension)
         Dimension dim = new Dimension(23.23, 34.34, 56.56);
         Product product = new Product(2, "1233", "chaise", 23.34, "C'est une chaise tavu :))))", 45.001, true, dim);
         ObjectMapper mapper = new ObjectMapper();
@@ -68,24 +156,6 @@ public class ProductController {
             product2 = new Product();
             product2.setDescription("WESSSSSSSH");
         }*/
-
-
-        //return jsonStr+"\n"+jsonInString;
-        //return product2.getDescription()+jsonInString+"\n\n"+jsonStr;
-        return productList.toString();
-    }
-
-    /*@RequestMapping("/products")
-    public List<Product> getProductList(){
-
-        RestTemplate restTemplate = new RestTemplate();
-        ProductList response = restTemplate.getForObject(
-                "http://factory.lunatech.fr/products",
-                ProductList.class);
-        List<Product> products = response.getProducts();
-
-        return products;
-    }*/
 
 
 }
