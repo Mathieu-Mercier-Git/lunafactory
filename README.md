1855d88fc0a67142966c43bb7360021e65a4a355
Mathieu-Mercir-Git <math_mercier@hotmail.fr>
2019-11-04 00:18:02 +0100

    This commit will add the bchanges to try to run the .jar file, Also add the readme file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lunafactory
+
+• To launch the project in the Terminal, go in the current directory that contain the .jar and type this command java -cp lunafactory.jar com.exercise.lunafactory.LunafactoryApplication
+
+• Type this url in the web browser : http://localhost:9090/Lunafactory
+
+• If the jar is running well you will be able to access the web application at the url specified above
--- src/main/java/com/exercise/lunafactory/LunafactoryApplication.java
+++ src/main/java/com/exercise/lunafactory/LunafactoryApplication.java
@@ -1,6 +1,7 @@
 package com.exercise.lunafactory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,5 +10,4 @@ public class LunafactoryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LunafactoryApplication.class, args);
 	}
-
 }
