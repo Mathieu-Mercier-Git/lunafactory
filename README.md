bb8049bf8a18980d9a0bd20e3ca9fb3218751cac
Mathieu-Mercir-Git <math_mercier@hotmail.fr>
2019-11-04 09:18:37 +0100

    This commit will add the jar file and the proper readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # lunafactory
 
-• To launch the project in the Terminal, go in the current directory that contain the .jar and type this command java -cp lunafactory.jar com.exercise.lunafactory.LunafactoryApplication
+• To launch the project in the Terminal, go in the main the directory .../lunafactory/ and execute this command : ./mvnw package
+
+• Then go in the current directory that contain the jar file. 
+In our case, go to .../lunafactory/target/ and type this command java -jar lunafactory-0.0.1-SNAPSHOT.jar
+The Spring Boot project will be launched.
 
 • Type this url in the web browser : http://localhost:9090/Lunafactory
 
-• If the jar is running well you will be able to access the web application at the url specified above
+• If the jar is running well you will be able to access the web application at the url specified above.
+You can now navigate on the website and check the lists by clicking the buttons on the home page
--- src/main/java/com/exercise/lunafactory/controller/ProductController.java
+++ src/main/java/com/exercise/lunafactory/controller/ProductController.java
@@ -8,14 +8,15 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.text.Normalizer;
 import java.util.*;
 
 @RestController
 public class ProductController {
 
+    //Value to change to keep or remove the duplicates in the list of products
     boolean removeDuplicates = true;
 
+    //current method called to show homepage of the web application
     @RequestMapping(value="/Lunafactory")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView("home");
@@ -23,6 +24,7 @@ public class ProductController {
         return modelAndView;
     }
 
+    //Method called to show the web page of the most expensive products
     @RequestMapping(value="/TopProducts")
     public ModelAndView greeting() {
         Map<String, Object> model = new HashMap<String, Object>();
@@ -32,10 +34,12 @@ public class ProductController {
         return modelAndView;
     }
 
+    //Method called to show the web page of the assembled expensive products
     @RequestMapping(value="/Products",method=RequestMethod.GET)
     public ModelAndView sortedProducts() {
         List<Product> sortedProductList = new ArrayList<>();
 
+        //We check the boolean value to know if we want to remove the duplicates
         if(removeDuplicates){
             sortedProductList=sortProductsWithNoDuplicates();
         } else {
@@ -49,6 +53,7 @@ public class ProductController {
         return modelAndView;
     }
 
+    //Method used to remove all products with Assembled value equal to false
     public List<Product> sortAssembledProducts() {
         List<Product> productList = productList();
         List<Product> sortedProductList = new ArrayList<>();
@@ -60,13 +65,13 @@ public class ProductController {
         return sortedProductList;
     }
 
+    //Method used to remove duplicates, checking if Product[i-1] has the same name
     public List<Product> sortProductsWithNoDuplicates() {
 
         List<Product> productList = productList();
         List<Product> sortedProductList = new ArrayList<>();
         for(int i=0;i<productList.size();i++){
             Product prod = productList.get(i);
-            System.out.println("NOM DU PRODUI : "+ Normalizer.normalize(productList.get(i).getName(), Normalizer.Form.NFD));
             if(prod.isAssembled()){
                 if(i==0){
                     sortedProductList.add(prod);
@@ -78,6 +83,7 @@ public class ProductController {
         }
         return sortedProductList;
     }
+
 
     public List<Product> topPrices(){
         List<Product> productList = new LinkedList<>();
@@ -107,9 +113,12 @@ public class ProductController {
         return maxPriceProd;
     }
 
+    //Make a get call on the RESTAPI
+    //We get a json with the list of products and we store it in a java List of Product objects
     public List<Product> productList(){
         RestTemplate restTemplate = new RestTemplate();
 
+        //Store the token to send it with the GET request
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-API-LunaFactory", "mathieu.mercier77360@gmail.com:12c71b271acfc1bf");
         HttpEntity<String> entity = new HttpEntity<>("body", headers);
